3d5dd9d374364117d7c75c0172e41829f16c3b0a
김수환/프로젝트1팀 <ksh60706@gmail.com>
2020-01-31 17:23:34 +0900

    1. collect/src : 국토교통부 Data 연결 API python 소스 생성. 2. collect/0.reference : 국토교통부 Data 관련 API 문서 정리. 3. master/src : 시군구코드 마스터 데이터 가져오기 python 소스 생성.

---
+++ master/src/getAddrCode.py
@@ -0,0 +1,21 @@
+#http://www.juso.go.kr/dn.do?reqType=ALLRDNM&regYmd=2019&ctprvnCd=00&gubun=RDNM&stdde=201912&fileName=201912_%EA%B1%B4%EB%AC%BCDB_%EC%A0%84%EC%B2%B4%EB%B6%84.zip&realFileName=201912ALLRDNM00.zip&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료
+
+
+'''
+------URL------
+http://www.juso.go.kr/dn.do?
+
+------Parameters------
+reqType : ALLRDNM
+reqYmd : 2019
+ctprvnCd : 00
+gubun : RDNM
+stdde : 201912
+fileName : 201912_건물DB_전체분.zip
+realFileName : 201912ALLRDNM00.zip
+indutyCd : 999
+purpsCd : 999
+indutyRm : 수집종료
+purpsRm : 수집종료
+'''
+
